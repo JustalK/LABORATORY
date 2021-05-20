25a0365b26dafbd3f5e5b9f815519f886e22b054
Latsuj <justal.kevin@gmail.com>
2021-05-20 20:06:54 +0800

    [FEATURE] Improving the loading system

--- JavaScript/React/src/App.js
+++ JavaScript/React/src/App.js
@@ -6,19 +6,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import ExperienceA from './ExperienceA'
-import ExperienceB from './ExperienceB'
-import ExperienceC from './ExperienceC'
-import ExperienceD from './ExperienceD'
-import ExperienceE from './ExperienceE'
-import ExperienceF from './ExperienceF'
-import ExperienceG from './ExperienceG'
-import ExperienceG2 from './ExperienceG2'
-import ExperienceH from './ExperienceH'
-import ExperienceI from './ExperienceI'
-import ExperienceJ from './ExperienceJ'
-import ExperienceK from './ExperienceK'
-import ExperienceL from './ExperienceL'
+import * as Experiences from './Experiences'
 
 function App() {
   return (
@@ -27,93 +15,25 @@ function App() {
         <div className="navigation">
           <nav>
             <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/experience_a">ExperienceA</Link>
-              </li>
-              <li>
-                <Link to="/experience_b">ExperienceB</Link>
-              </li>
-              <li>
-                <Link to="/experience_c">ExperienceC</Link>
-              </li>
-              <li>
-                <Link to="/experience_d">ExperienceD</Link>
-              </li>
-              <li>
-                <Link to="/experience_e">ExperienceE</Link>
-              </li>
-              <li>
-                <Link to="/experience_f">ExperienceF</Link>
-              </li>
-              <li>
-                <Link to="/experience_g">ExperienceG</Link>
-              </li>
-              <li>
-                <Link to="/experience_g2">ExperienceG2</Link>
-              </li>
-              <li>
-                <Link to="/experience_h">ExperienceH</Link>
-              </li>
-              <li>
-                <Link to="/experience_i">ExperienceI</Link>
-              </li>
-              <li>
-                <Link to="/experience_j">ExperienceJ</Link>
-              </li>
-              <li>
-                <Link to="/experience_k">ExperienceK</Link>
-              </li>
-              <li>
-                <Link to="/experience_l">ExperienceL</Link>
-              </li>
+              {Object.keys(Experiences).map((e, index) => (
+                <li>
+                  <Link key={index} to={"/" + e}>{e}</Link>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
         <div className="content">
           <Switch>
-            <Route path="/experience_a">
-              <ExperienceA />
-            </Route>
-            <Route path="/experience_b">
-              <ExperienceB />
-            </Route>
-            <Route path="/experience_c">
-              <ExperienceC />
-            </Route>
-            <Route path="/experience_d">
-              <ExperienceD />
-            </Route>
-            <Route path="/experience_e">
-              <ExperienceE />
-            </Route>
-            <Route path="/experience_f">
-              <ExperienceF />
-            </Route>
-            <Route path="/experience_g">
-              <ExperienceG />
-            </Route>
-            <Route path="/experience_g2">
-              <ExperienceG2 />
-            </Route>
-            <Route path="/experience_h">
-              <ExperienceH />
-            </Route>
-            <Route path="/experience_i">
-              <ExperienceI />
-            </Route>
-            <Route path="/experience_j">
-              <ExperienceJ />
-            </Route>
-            <Route path="/experience_k">
-              <ExperienceK />
-            </Route>
-            <Route path="/experience_l">
-              <ExperienceL />
-            </Route>
+            {Object.keys(Experiences).map((e, index) => {
+              const Type = Experiences[e];
+              return (
+                <Route path={"/" + e}>
+                  <Type />
+                </Route>
+            )})}
             <Route path="/">
+              <Experiences.ExperienceA />
             </Route>
           </Switch>
         </div>
---
+++ JavaScript/React/src/Experiences/index.js
@@ -0,0 +1,29 @@
+import ExperienceA from './ExperienceA'
+import ExperienceB from './ExperienceB'
+import ExperienceC from './ExperienceC'
+import ExperienceD from './ExperienceD'
+import ExperienceE from './ExperienceE'
+import ExperienceF from './ExperienceF'
+import ExperienceG from './ExperienceG'
+import ExperienceG2 from './ExperienceG2'
+import ExperienceH from './ExperienceH'
+import ExperienceI from './ExperienceI'
+import ExperienceJ from './ExperienceJ'
+import ExperienceK from './ExperienceK'
+import ExperienceL from './ExperienceL'
+
+export {
+  ExperienceA,
+  ExperienceB,
+  ExperienceC,
+  ExperienceD,
+  ExperienceE,
+  ExperienceF,
+  ExperienceG,
+  ExperienceG2,
+  ExperienceH,
+  ExperienceI,
+  ExperienceJ,
+  ExperienceK,
+  ExperienceL
+}
