5f169b329a917d99f4ecfe802462ee1a98471258
Latsuj <justal.kevin@gmail.com>
2021-06-22 18:46:50 +0800

    [FEATURE] Learning how to use the router

--- JavaScript/Angular/Tutorials/src/app/app-routing.module.ts
+++ JavaScript/Angular/Tutorials/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
