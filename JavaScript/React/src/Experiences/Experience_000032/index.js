4fd0e7aba73606941a4320cc2a27e512404af0b2
Latsuj <justal.kevin@gmail.com>
2021-06-28 19:20:16 +0800

    [FEATURE] Playing with uncontrolled input

--- JavaScript/React/src/Experiences/Experience_000032/index.js
+++ JavaScript/React/src/Experiences/Experience_000032/index.js
@@ -3,6 +3,8 @@ import React, { Profiler, useState, useRef } from "react"
 const Experience = () => {
   const [val, setVal] = useState(0)
   const ref = useRef(0)
+  const refInput = useRef('')
+  const [valInput, setValInput] = useState('')
 
   const callback = (id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
     console.log(id, phase, actualDuration, baseDuration, startTime, commitTime, interactions)
@@ -18,6 +20,14 @@ const Experience = () => {
         <span>{val}</span>
         <button onClick={() => ref.current += 1}>Increment the useRef and look console</button>
         <span>{ref.current}</span>
+        <span>
+          Below, it's certainly the most interesting use of it. useRef will not trigger a re-render, so if we dont need the value until we click on the button, it's better than useState (Uncontrolled state)
+        </span>
+        <span>Input with setState</span>
+        <input onChange={(e) => setValInput(e.target.value)} /><br />
+        <span>Input with ref</span>
+        <input onChange={(e) => refInput.current=e.target.value} />
+        <button onClick={() => console.log('ref:' + refInput.current, 'state:' + valInput)}>Get value in console</button>
       </div>
     </Profiler>
   );
