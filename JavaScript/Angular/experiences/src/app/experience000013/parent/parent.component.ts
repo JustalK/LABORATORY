80f454c9c854fb76fa4ce7e67b94a2bc67703f91
Latsuj <justal.kevin@gmail.com>
2021-07-04 23:41:47 +0800

    [FEATURE] Adding experience on build in directive angular

--- JavaScript/Angular/experiences/src/app/experience000013/parent/parent.component.ts
+++ JavaScript/Angular/experiences/src/app/experience000013/parent/parent.component.ts
@@ -8,18 +8,31 @@ import { Component } from '@angular/core';
 export default class ParentComponent {
   class = false
   styleActivator = false
+  numbers = [1, 2, 3]
+  switchCase = 1
+  isChildrenActive = true
   style = {}
   currentItem = {
     name: 'test'
   }
+
+  changeChildrenActive() {
+    this.isChildrenActive = !this.isChildrenActive
+  }
+
   changeClass() {
     this.class = !this.class
   }
+
   changeStyle() {
     this.styleActivator = !this.styleActivator
     this.style = {
       'font-size':   this.styleActivator    ? '24px'   : '12px'
     };
+  }
+
+  incrementCase() {
+    this.switchCase += 1;
   }
 
   onChange() {
