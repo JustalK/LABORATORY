d321a9ff40959db46ca78f1887abb55625a26e82
Latsuj <justal.kevin@gmail.com>
2021-07-12 23:19:59 +0800

    [FEATURE] Testing template variable

--- JavaScript/Angular/experiences/src/app/experience000016/parent/parent.component.ts
+++ JavaScript/Angular/experiences/src/app/experience000016/parent/parent.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
 })
 export default class ParentComponent {
   currentCustomer = 'Maria';
+
+  callConsole(value: string) {
+    console.log(value);
+  }
 }
