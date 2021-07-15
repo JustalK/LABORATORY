382ae3c1343e7b4e7492e9e9471d415844097467
Latsuj <justal.kevin@gmail.com>
2021-07-15 23:56:03 +0800

    [FEATURE] Linking the a to b

--- Mixed/React-Three-Fiber-Motion/src/Experiences/Experience_000001/index.js
+++ Mixed/React-Three-Fiber-Motion/src/Experiences/Experience_000001/index.js
@@ -1,10 +1,18 @@
+import { useLocation } from "wouter";
 
 export default function Transition() {
+  const [location, setLocation] = useLocation();
+  console.log(location)
+
+  const handleClick = () => {
+    setLocation("/b")
+  }
+
   return (
     <>
       <ambientLight intensity={0.1} />
       <directionalLight color="pink" position={[0, 0, 5]} />
-      <mesh position={[0, 0, 0 ]}>
+      <mesh position={[0, 0, 0 ]} onClick={handleClick}>
         <boxGeometry />
         <meshPhongMaterial />
       </mesh>
